600d5887744cd4eb6c75da7c2a5c8492276e026d
28kayak <kaya_ota@msn.com>
2015-04-23 18:29:44 -0700

    edit

--- prologClassDemo.pl
+++ prologClassDemo.pl
@@ -1,5 +1,13 @@
+%Q:myAppend(3,[],R).
+%false 
+myAppend(X,[],[X]).
+%Q:myAppend(3,[],R).
+%R = [3]
+%false. how can we prevent this false?
 myAppend(X,[H|T], [X, H|T]).
 %Q: myAppend(3,[1,2,5],R)
 %R = [3,1,2,5]
-%Q:myAppend(3,[],R).
-%false 
+myAppend(X,[],[X]) :- !.
+
+
+
